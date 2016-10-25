d4fec0ebda0967d799811b4b880e57b2079e6e26
otacke <o.tacke@web.de>
2016-10-25 20:27:25 +0200

    add first code and notebook

--- notebooks/smartcab/smartcab/agent.py
+++ notebooks/smartcab/smartcab/agent.py
@@ -10,7 +10,9 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        
         # TODO: Initialize any additional variables here
+        self.state = None
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -24,15 +26,21 @@ class LearningAgent(Agent):
 
         # TODO: Update state
         
+        # Use a Dictionary to store the state
+        self.state = inputs
+        self.state['next_waypoint'] = self.next_waypoint
+        
         # TODO: Select action according to your policy
-        action = None
+
+        # random action
+        action = random.choice(Environment.valid_actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
 def run():
@@ -41,11 +49,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
